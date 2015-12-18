dc053c926e90429a6ce07a15bb77335e6640f92e
Dmitry Sinyov <mifist001@gmail.com>
2015-12-18 18:15:23 +0300

    Start project.

---
+++ TestSimpleSystems/TestSimpleSystems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSimpleSystems
+//
+//  Created by Дмитрий on 18.12.15.
+//  Copyright © 2015 DmitrySinyov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSimpleSystems/TestSimpleSystems/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSimpleSystems
+//
+//  Created by Дмитрий on 18.12.15.
+//  Copyright © 2015 DmitrySinyov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
