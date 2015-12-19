e5072dde036204f210303694c6a469240007373b
Dmitry Sinyov <mifist001@gmail.com>
2015-12-19 23:27:15 +0300

    Map work.

--- TestSimpleSystems/TestSimpleSystems/MainViewController.m
+++ TestSimpleSystems/TestSimpleSystems/MainViewController.m
@@ -16,7 +16,14 @@
 
 #import "PointsManager.h"
 
-@interface MainViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+@interface AnnotationWithPointID : MKPointAnnotation {
+}
+@property (nonatomic, retain) NSString *pointID;
+@end
+@implementation AnnotationWithPointID
+@end
+
+@interface MainViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, MKMapViewDelegate, CLLocationManagerDelegate>
 @property (strong, nonatomic) IBOutlet UISegmentedControl *listMapController;
 @property (strong, nonatomic) IBOutlet UITableView *pointsTable;
 @property (strong, nonatomic) IBOutlet MKMapView *pointsMap;
@@ -29,9 +36,11 @@
     NSMutableArray <SomePoint*> *allPoints;
     bool isTableActive;
     bool addPoint;
-    NSIndexPath *choosedCell;
+    NSInteger choosedCell;
     
     bool tryDeletePoint;
+    
+    CLLocationManager *locationManager;
 }
 
 - (void)viewDidLoad {
@@ -81,6 +90,17 @@
                                                  name:NOTIFICATION_DELETE_POINT_FAILED
                                                object:nil];
     
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+
+#warning только 1 из этих методов использовать
+    [locationManager requestWhenInUseAuthorization];//только foreground
+    //[locationManager requestAlwaysAuthorization];//и background тоже
+    
+    [locationManager startUpdatingLocation];
+    
+    _pointsMap.delegate = self;
+    
 //    SomePoint *testPoint = [[SomePoint alloc] init];
 //    testPoint.pointID = @"1";
 //    testPoint.title = @"title";
@@ -93,7 +113,9 @@
 
 -(void)receiveAllPointsSuccess
 {
-    [self updateInterface];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self updateInterface];
+    });
 }
 
 -(void)receiveAllPointsFail
@@ -109,24 +131,32 @@
 
 -(void)receiveAddPointSuccess
 {
-    [self updateInterface];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self updateInterface];
+    });
 }
 
 #warning при необходимости можно переделать, чтобы не обновлять всё при изменении одной точки
 -(void)receiveGetFullPointSuccess
 {
-    [self updateInterface];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self updateInterface];
+    });
 }
 
 -(void)receiveUpdatePointSuccess
 {
-    [self updateInterface];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self updateInterface];
+    });
 }
 
 -(void)receiveDeletePointsSuccess
 {
-    [self updateInterface];
-    tryDeletePoint = NO;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self updateInterface];
+        tryDeletePoint = NO;
+    });
 }
 -(void)receiveDeletePointsFail
 {
@@ -145,15 +175,31 @@
 
 -(void)updateInterface
 {
-#warning сортировка точек
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [allPoints removeAllObjects];
-        [allPoints addObjectsFromArray:[[PointsManager sharedInstance] getAllPoints]];
-        
-        [_pointsTable reloadData];
-    });
+    [allPoints removeAllObjects];
+    [allPoints addObjectsFromArray:[[PointsManager sharedInstance] getAllPoints]];
     
-#warning обновить карту тоже
+    CLLocationCoordinate2D currentCoord = _pointsMap.userLocation.location.coordinate;
+    [_pointsMap setCenterCoordinate:currentCoord animated:YES];
+    
+    //NSLog(@"current coord %f %f", currentCoord.latitude, currentCoord.longitude);
+    [allPoints sortUsingComparator:^NSComparisonResult(SomePoint* point1, SomePoint* point2) {
+        NSNumber* distance1 = [NSNumber numberWithDouble:[_pointsMap.userLocation.location distanceFromLocation:[[CLLocation alloc] initWithLatitude:point1.lat longitude:point1.lng]]];
+        NSNumber* distance2 = [NSNumber numberWithDouble:[_pointsMap.userLocation.location distanceFromLocation:[[CLLocation alloc] initWithLatitude:point2.lat longitude:point2.lng]]];
+        return [distance2 compare:distance1];
+    }];
+    
+    [_pointsTable reloadData];
+    
+//обновить карту
+    [_pointsMap removeAnnotations:_pointsMap.annotations];
+    for (SomePoint *point in allPoints)
+    {
+        AnnotationWithPointID *annotation = [[AnnotationWithPointID alloc] init];
+        [annotation setCoordinate:CLLocationCoordinate2DMake(point.lat, point.lng)];
+        [annotation setTitle:point.title];
+        [annotation setPointID:point.pointID];
+        [_pointsMap addAnnotation:annotation];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -213,7 +259,7 @@
     [[PointsManager sharedInstance] loadAllPoints];
 }
 
-//delegate datasource
+//tableview delegate datasource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
@@ -240,7 +286,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     addPoint = NO;
-    choosedCell = indexPath;
+    choosedCell = indexPath.row;
     
     //[[PointsManager sharedInstance] getFullPointWithID:[allPoints objectAtIndex:indexPath.row].pointID];
     
@@ -261,6 +307,44 @@
     }
 }
 
+//mapview delegate
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+    
+    MKAnnotationView *annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"location"];
+    annotationView.canShowCallout = YES;
+    annotationView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    
+    return annotationView;
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    NSLog(@"callout tapped for %@", view.annotation.title);
+    
+    for (int i = 0; i < allPoints.count; i++)
+    {
+        SomePoint *point = [allPoints objectAtIndex:i];
+        if ([point.pointID isEqualToString:((AnnotationWithPointID*)view.annotation).pointID])
+        {
+            choosedCell = i;
+            addPoint = NO;
+            [self performSegueWithIdentifier:@"addEditPoint" sender:nil];
+            break;
+        }
+    }
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+#warning проверить и заменить при необходимости
+    //MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    //[_pointsMap setRegion:[_pointsMap regionThatFits:region] animated:YES];
+}
+
+//navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"addEditPoint"])
@@ -269,7 +353,7 @@
         aepVC.isAdding = addPoint;
         if (!addPoint)
         {
-            aepVC.editingPoint = [allPoints objectAtIndex:choosedCell.row];
+            aepVC.editingPoint = [allPoints objectAtIndex:choosedCell];
         }
     }
 }
